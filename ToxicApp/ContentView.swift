9d999302349b2598e5aa3a8fe921cd3a20202e2d
German <lordireality@gmail.com>
2024-01-13 23:44:34 +0300

    Initial Commit

---
+++ ToxicApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToxicApp
+//
+//  Created by Герман Зыкин on 13.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
